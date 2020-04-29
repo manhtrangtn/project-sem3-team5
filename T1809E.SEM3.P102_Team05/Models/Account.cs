2bbb5ef86b169313ace712ed814f321430798bf6
QuangTrung6620 <50941864+QuangTrung6620@users.noreply.github.com>
2020-04-29 08:52:36 +0700

    dataseeding

--- T1809E.SEM3.P102_Team05/Models/Account.cs
+++ T1809E.SEM3.P102_Team05/Models/Account.cs
@@ -8,5 +8,23 @@ namespace T1809E.SEM3.P102_Team05.Models
 {
     public class Account: IdentityUser
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public DateTime BirthDay { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public AccountStatus Status { get; set; }
+    }
+    public enum AccountStatus
+    {
+        ACTIVE = 0,
+        DISABLE = 1,
+        OUT_OF_STOCK = -1,
+        DELETED = -2
     }
 }
