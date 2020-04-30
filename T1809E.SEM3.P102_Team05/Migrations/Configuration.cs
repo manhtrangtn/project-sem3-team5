a453bd9f334b2a0b8d5433c6e393b37591df1ef9
QuangTrung6620 <50941864+QuangTrung6620@users.noreply.github.com>
2020-04-30 09:46:50 +0700

    update dataseeding

--- T1809E.SEM3.P102_Team05/Migrations/Configuration.cs
+++ T1809E.SEM3.P102_Team05/Migrations/Configuration.cs
@@ -4,6 +4,7 @@
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using T1809E.SEM3.P102_Team05.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<T1809E.SEM3.P102_Team05.Data.AppDatabaseContext>
     {
@@ -25,7 +26,7 @@
                 UpdateAt = DateTime.Parse("2019-01-15"),
                 DeleteAt = DateTime.Parse("2019-02-03"),
                 InStock = 1,
-                Status.ACTIVE
+                OrderStatus.ACTIVE
             },
             new Product()
             {
@@ -37,7 +38,7 @@
                 UpdateAt = DateTime.Parse("2019-01-16"),
                 DeleteAt = DateTime.Parse("2019-02-04"),
                 InStock = 2,
-                Status.ACTIVE
+                OrderStatus.ACTIVE
             },
             new Product()
             {
@@ -49,7 +50,7 @@
                 UpdateAt = DateTime.Parse("2019-01-17"),
                 DeleteAt = DateTime.Parse("2019-02-07"),
                 InStock = 3,
-                Status.DISABLED
+                OrderStatus.DISABLED
             }, new Product()
             {
                 Id = 4,
@@ -60,7 +61,7 @@
                 UpdateAt = DateTime.Parse("2019-01-18"),
                 DeleteAt = DateTime.Parse("2019-02-06"),
                 InStock = 4,
-                Status.ACTIVE
+                OrderStatus.ACTIVE
             },
             new Product()
             {
@@ -72,7 +73,7 @@
                 UpdateAt = DateTime.Parse("2019-01-20"),
                 DeleteAt = DateTime.Parse("2019-02-13"),
                 InStock = 3,
-                Status.ACTIVE
+                OrderStatus.ACTIVE
             },
             new Product()
             {
@@ -84,7 +85,7 @@
                 UpdateAt = DateTime.Parse("2019-01-21"),
                 DeleteAt = DateTime.Parse("2019-02-05"),
                 InStock = 4,
-                Status.DISABLED
+                OrderStatus.DISABLED
             },
             new Product()
             {
@@ -96,7 +97,7 @@
                 UpdateAt = DateTime.Parse("2019-01-25"),
                 DeleteAt = DateTime.Parse("2019-02-23"),
                 InStock = 4,
-                Status.ACTIVE
+                OrderStatus.ACTIVE
             },
             new Product()
             {
@@ -108,7 +109,7 @@
                 UpdateAt = DateTime.Parse("2019-03-15"),
                 DeleteAt = DateTime.Parse("2019-03-24"),
                 InStock = 19,
-                Status.ACTIVE
+                OrderStatus.ACTIVE
             },
             new Product()
             {
@@ -120,7 +121,7 @@
                 UpdateAt = DateTime.Parse("2019-03-15"),
                 DeleteAt = DateTime.Parse("2019-04-03"),
                 InStock = 40,
-                Status.DISABLED
+                OrderStatus.DISABLED
             },
             new Product()
             {
@@ -132,7 +133,247 @@
                 UpdateAt = DateTime.Parse("2019-01-25"),
                 DeleteAt = DateTime.Parse("2019-02-30"),
                 InStock = 4,
-                Status.DISABLED
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 11,
+                Name = "Áo phông nam",
+                Thumbnail = "https://shopmc.vn/wp-content/uploads/2017/03/Ao-phong-BHGaming-Minecraft.jpg",
+                Price = 55000,
+                CreateAt = DateTime.Parse("2020-01-01"),
+                UpdateAt = DateTime.Parse("2020-01-03"),
+                DeleteAt = DateTime.Parse("2020-02-05"),
+                InStock = 70,
+                OrderStatus.OUT_OF_STOCK
+            },
+            new Product()
+            {
+                Id = 12,
+                Name = "Áo phông nữ",
+                Thumbnail = "https://kenhphunu.com/media/102015/1559754000/ao_phong_trang.jpg",
+                Price = 55000,
+                CreateAt = DateTime.Parse("2019-04-13"),
+                UpdateAt = DateTime.Parse("2019-04-15"),
+                DeleteAt = DateTime.Parse("2019-04-20"),
+                InStock = 80,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 13,
+                Name = "quần Baggy",
+                Thumbnail = "https://nocturnal.vn/wp-content/uploads/2019/09/3-600x600.jpg",
+                Price = 115000,
+                CreateAt = DateTime.Parse("2019-04-15"),
+                UpdateAt = DateTime.Parse("2019-04-19"),
+                DeleteAt = DateTime.Parse("2019-05-27"),
+                InStock = 100,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 14,
+                Name = "Áo Hoodie",
+                Thumbnail = "https://media3.scdn.vn/img3/2019/11_2/SQIy05_simg_de2fe0_500x500_maxb.jpg",
+                Price = 135000,
+                CreateAt = DateTime.Parse("2019-05-05"),
+                UpdateAt = DateTime.Parse("2019-05-10"),
+                DeleteAt = DateTime.Parse("2019-05-30"),
+                InStock = 80,
+                OrderStatus.OUT_OF_STOCK
+            },
+            new Product()
+            {
+                Id = 15,
+                Name = "Áo Thun nữ",
+                Thumbnail = "https://aothuntrongiasi.com/wp-content/uploads/2018/04/ao-thun-trang-nu-1.jpg",
+                Price = 75000,
+                CreateAt = DateTime.Parse("2019-06-13"),
+                UpdateAt = DateTime.Parse("2019-06-29"),
+                DeleteAt = DateTime.Parse("2019-07-23"),
+                InStock = 110,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 16,
+                Name = "quần short nam",
+                Thumbnail = "https://img.zanado.com/media/catalog/product/cache/all/thumbnail/360x420/7b8fef0172c2eb72dd8fd366c999954c/7/_/quan_short_nam_tron_pa_e7c5.jpg",
+                Price = 95000,
+                CreateAt = DateTime.Parse("2019-06-03"),
+                UpdateAt = DateTime.Parse("2019-06-15"),
+                DeleteAt = DateTime.Parse("2019-07-03"),
+                InStock = 60,
+                OrderStatus.DELETED
+            },
+            new Product()
+            {
+                Id = 17,
+                Name = "Quần áo thể thao",
+                Thumbnail = "https://media3.scdn.vn/img2/2018/5_3/bdEfl8_simg_de2fe0_500x500_maxb.jpg",
+                Price = 125000,
+                CreateAt = DateTime.Parse("2019-07-01"),
+                UpdateAt = DateTime.Parse("2019-07-15"),
+                DeleteAt = DateTime.Parse("2019-07-23"),
+                InStock = 140,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 18,
+                Name = "Áo dạ nam",
+                Thumbnail = "https://media3.scdn.vn/img3/2019/10_3/eDADlp_simg_de2fe0_500x500_maxb.png",
+                Price = 450000,
+                CreateAt = DateTime.Parse("2019-07-13"),
+                UpdateAt = DateTime.Parse("2019-08-29"),
+                DeleteAt = DateTime.Parse("2019-09-03"),
+                InStock = 10,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 19,
+                Name = "Áo dạ nữ",
+                Thumbnail = "https://media3.scdn.vn/img3/2018/12_22/tXJbuH_simg_de2fe0_500x500_maxb.jpg",
+                Price = 455000,
+                CreateAt = DateTime.Parse("2020-02-13"),
+                UpdateAt = DateTime.Parse("2020-02-15"),
+                DeleteAt = DateTime.Parse("2020-04-03"),
+                InStock = 17,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 20,
+                Name = "Áo Phao",
+                Thumbnail = "https://dosityna.com/wp-content/uploads/2019/01/ao-phao-dep.jpg",
+                Price = 355000,
+                CreateAt = DateTime.Parse("2019-11-30"),
+                UpdateAt = DateTime.Parse("2019-12-15"),
+                DeleteAt = DateTime.Parse("2019-12-23"),
+                InStock = 30,
+                OrderStatus.ACTIVE
+            },
+            new Product()
+            {
+                Id = 21,
+                Name = "Quần jogger nam",
+                Thumbnail = "https://quanjogger.vn/image/cache/catalog/quanjoggernam/kaki/500-620-quan-jogger-nam-xam-tieu-avatar-500x620.jpg",
+                Price = 85000,
+                CreateAt = DateTime.Parse("2019-10-13"),
+                UpdateAt = DateTime.Parse("2019-10-18"),
+                DeleteAt = DateTime.Parse("2019-10-23"),
+                InStock = 190,
+                OrderStatus.ACTIVE
+            },
+            new Product()
+            {
+                Id = 22,
+                Name = "quần jogger nữ",
+                Thumbnail = "https://ann.com.vn/wp-content/uploads/11017-clean-857494ffd81821467809.jpg",
+                Price = 85000,
+                CreateAt = DateTime.Parse("2019-08-01"),
+                UpdateAt = DateTime.Parse("2019-08-15"),
+                DeleteAt = DateTime.Parse("2019-09-23"),
+                InStock = 180,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 23,
+                Name = "Áo sơ mi caro",
+                Thumbnail = "https://file.hstatic.net/1000401164/file/ao-so-mi-nam-thoi-trang-mando-1_d129dc6d1a28427ead3f29bbb81d2225_grande.jpg",
+                Price = 125000,
+                CreateAt = DateTime.Parse("2020-02-06"),
+                UpdateAt = DateTime.Parse("2020-02-07"),
+                DeleteAt = DateTime.Parse("2020-04-03"),
+                InStock = 240,
+                OrderStatus.OUT_OF_STOCK
+            },
+            new Product()
+            {
+                Id = 24,
+                Name = "Áo len dài tay",
+                Thumbnail = "https://vcdn-ngoisao.vnecdn.net/2018/12/21/Ao-len-dai-tay-nam-Aristino-AW-3796-5251-1545389942.jpg",
+                Price = 145000,
+                CreateAt = DateTime.Parse("2019-08-18"),
+                UpdateAt = DateTime.Parse("2019-09-15"),
+                DeleteAt = DateTime.Parse("2019-10-03"),
+                InStock = 120,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 25,
+                Name = "Váy len",
+                Thumbnail = "https://media3.scdn.vn/img2/2018/9_14/s52GxR_simg_de2fe0_500x500_maxb.jpg",
+                Price = 95000,
+                CreateAt = DateTime.Parse("2020-02-13"),
+                UpdateAt = DateTime.Parse("2020-03-15"),
+                DeleteAt = DateTime.Parse("2020-04-03"),
+                InStock = 48,
+                OrderStatus.ACTIVE
+            },
+            new Product()
+            {
+                Id = 26,
+                Name = "Quần vải nam",
+                Thumbnail = "https://ragus.vn/wp-content/uploads/2019/04/Qu%E1%BA%A7n-V%E1%BA%A3i-Nam-Woven-%C4%91en-QV125007-2.jpg",
+                Price = 125000,
+                CreateAt = DateTime.Parse("2020-02-23"),
+                UpdateAt = DateTime.Parse("2020-03-29"),
+                DeleteAt = DateTime.Parse("2020-04-17"),
+                InStock = 63,
+                OrderStatus.ACTIVE
+            },
+            new Product()
+            {
+                Id = 27,
+                Name = "Quần áo cho bé",
+                Thumbnail = "https://thoitrang.biz/wp-content/uploads/2014/12/bo-ao-lien-quan-hinh-thu-de-thuong-cho-cac-be-1.jpg",
+                Price = 90000,
+                CreateAt = DateTime.Parse("2019-09-23"),
+                UpdateAt = DateTime.Parse("2019-10-16"),
+                DeleteAt = DateTime.Parse("2019-10-23"),
+                InStock = 54,
+                OrderStatus.ACTIVE
+            },
+            new Product()
+            {
+                Id = 28,
+                Name = "Quần áo thun",
+                Thumbnail = "https://www.bexinhshop.vn/image/data/17-1-2019/setdaydeonoCARO.jpg",
+                Price = 120000,
+                CreateAt = DateTime.Parse("2019-09-11"),
+                UpdateAt = DateTime.Parse("2019-09-25"),
+                DeleteAt = DateTime.Parse("2019-11-23"),
+                InStock = 23,
+                OrderStatus.DISABLED
+            },
+            new Product()
+            {
+                Id = 29,
+                Name = "Áo thu đông",
+                Thumbnail = "https://mjumju.net/wp-content/uploads/2017/12/ao-thu-dong-nam-TD014-3.jpg",
+                Price = 100000,
+                CreateAt = DateTime.Parse("2020-01-14"),
+                UpdateAt = DateTime.Parse("2020-01-25"),
+                DeleteAt = DateTime.Parse("2020-02-12"),
+                InStock = 61,
+                OrderStatus.OUT_OF_STOCK
+            },
+            new Product()
+            {
+                Id = 30,
+                Name = "Áo nỉ nam",
+                Thumbnail = "https://linhvnxk.com/wp-content/uploads/2018/10/ao-ni-nam-xuat-han-tran-tram-15.jpg",
+                Price = 115000,
+                CreateAt = DateTime.Parse("2019-12-13"),
+                UpdateAt = DateTime.Parse("2020-01-15"),
+                DeleteAt = DateTime.Parse("2020-02-03"),
+                InStock = 89,
+                OrderStatus.DISABLED
             });
         }
     }
