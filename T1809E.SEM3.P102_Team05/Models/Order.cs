4a03eb83e8140e2526057ce091f0ddd214ee4bde
NGuyen Trung Sy <syntth1808031@fpt.edu.vn>
2020-05-01 16:41:45 +0700

    entity

--- T1809E.SEM3.P102_Team05/Models/Order.cs
+++ T1809E.SEM3.P102_Team05/Models/Order.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+<<<<<<< Updated upstream
 using System.ComponentModel.DataAnnotations;
+=======
+>>>>>>> Stashed changes
 using System.Linq;
 using System.Web;
 
@@ -8,6 +11,7 @@ namespace T1809E.SEM3.P102_Team05.Models
 {
     public class Order
     {
+<<<<<<< Updated upstream
         [Key]
         public int OrderId { get; set; }
         public int CustomerID { get; set; }
@@ -24,5 +28,25 @@ namespace T1809E.SEM3.P102_Team05.Models
         ACTIVE = 0,
         DISABLE = 1,
         DELETED = -1
+=======
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public string OrderNumber { get; set; }
+        public string ShipAddress { get; set; }
+        public double Total { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public DateTime DeleteAt { get; set; }
+        
+    }
+    public enum OrderStatus1
+    {
+        UNPAID = 0,
+        CANCELED = 1,
+        DELETED = 2,
+        DONE =  3,
+        PAID = 4,
+        SHIPPING=5,
+>>>>>>> Stashed changes
     }
 }
